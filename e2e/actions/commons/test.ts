30abf0e9ffb3e5c198e0a7daa25edddbd42df119
Hiếu Văn Trần <hieu.tran@enouvo.com>
2019-05-07 18:22:55 +0700

    update test case

--- e2e/actions/commons/test.ts
+++ e2e/actions/commons/test.ts
@@ -1,16 +1,99 @@
 import { browser, ElementFinder, protractor } from 'protractor';
 
-export class base {
+export class basePage {
     waitForElementAndSendkeys: Function;
+    waitForElementAndClick: Function;
+    waitForElementAndGetText: Function;
+    waitForElementToCheckContainText: Function;
+    getElementValue: Function;
+    switchIframe: Function;
+    switchParrentWindow: Function;
+    popUpCookies: ElementFinder;
+    closePopUpCookies: Function;
+    waitForUrlAndAssert: Function;
+    clearCache: Function;
+    clearAPIUatUrl: string;
+    clearAPIUat: ElementFinder;
+    checkElementVisible: Function;
     constructor() {
         this.waitForElementAndSendkeys = async (elementWait: ElementFinder, text: string) => {
             let EC = protractor.ExpectedConditions;
             await browser.wait(EC.visibilityOf(elementWait), 100000).then(async () => {
                 await elementWait.sendKeys(text);
-
             })
         }
-    }
+        this.waitForElementAndClick = async (elementWait: ElementFinder) => {
+            let EC = protractor.ExpectedConditions;
+            await browser.wait(EC.invisibilityOf(elementWait), 100000);
+            await browser.wait(EC.presenceOf(elementWait), 100000);
+            await browser.wait(EC.visibilityOf(elementWait), 100000);
+            await browser.wait(EC.elementToBeClickable(elementWait), 100000);
+            await browser.sleep(500);
+            await elementWait.click();
+        }
+        this.waitForElementAndGetText = async (elementWait: ElementFinder, result: string) => {
+            let EC = protractor.ExpectedConditions;
+            await browser.wait(EC.invisibilityOf(elementWait), 100000);
+            await browser.wait(EC.visibilityOf(elementWait), 100000);
+            await elementWait.getText().then(function (text) {
+                expect(text).toEqual(result);
+            })
+        }
+        this.waitForElementToCheckContainText = async (elementWait: ElementFinder, result: string) => {
+            let EC = protractor.ExpectedConditions;
+            await browser.wait(EC.invisibilityOf(elementWait), 100000);
+            await browser.wait(EC.visibilityOf(elementWait), 100000);
+            await elementWait.getText().then(function (text) {
+                expect(text).toContain(result)
+            })
+        }
+        //function for only check Reebonz email user
+        this.getElementValue = async (elementWait: ElementFinder) => {
+            let EC = protractor.ExpectedConditions;
+            await browser.wait(EC.invisibilityOf(elementWait), 100000);
+            await browser.wait(EC.visibilityOf(elementWait), 100000);
+            let result = await elementWait.getAttribute("value");
 
+            return result;
+        }
+        this.switchIframe = async (elementWait: ElementFinder) => {
+            let EC = protractor.ExpectedConditions;
+            await browser.wait(EC.invisibilityOf(elementWait), 100000);
+            await browser.wait(EC.visibilityOf(elementWait), 100000);
+            await browser.switchTo().frame(elementWait.getWebElement());
+        }
+        this.switchParrentWindow = async (elementWait: ElementFinder) => {
+            let EC = protractor.ExpectedConditions;
+            await browser.wait(EC.invisibilityOf(elementWait), 100000);
+            await browser.wait(EC.visibilityOf(elementWait), 100000);
+            await browser.switchTo().defaultContent();
+
+        }
+
+        this.closePopUpCookies = async (elementWait: ElementFinder) => {
+            let EC = protractor.ExpectedConditions;
+            let popup = await browser.wait(EC.visibilityOf(elementWait), 50000);
+            if (popup) {
+                elementWait.click();
+            }
+        }
+        this.waitForUrlAndAssert = async (string: string) => {
+            let EC = protractor.ExpectedConditions;
+            await browser.wait(EC.urlContains(string), 100000);
+        }
+        this.clearCache = async (url: string) => {
+            await browser.get(url);
+            await browser.executeScript('window.localStorage.clear();');
+            await browser.executeScript('window.sessionStorage.clear();');
+            await browser.driver.manage().deleteAllCookies();
+        }
+
+        this.checkElementVisible = async (elementWait: ElementFinder) => {
+            let EC = protractor.ExpectedConditions;
+            await browser.wait(EC.invisibilityOf(elementWait), 100000);
+            await browser.wait(EC.presenceOf(elementWait), 100000);
+            await browser.wait(EC.visibilityOf(elementWait), 100000);
+        }
+    }
 
 }
--- e2e/stepdefinitions/loginSteps.spec.ts
+++ e2e/stepdefinitions/loginSteps.spec.ts
@@ -2,33 +2,31 @@ import {$, browser, By, protractor, element, ElementFinder, by} from 'protractor
 import { LoginPage } from '../actions/pages/loginPage';
 import { defineSupportCode } from 'cucumber';
 import {} from 'jasmine';
-import { base } from "../actions/commons/test";
+import { basePage } from "../actions/commons/test";
 import { async } from '@angular/core/testing';
 const chai = require('chai').use(require('chai-as-promised'));
 const expect = chai.expect;
 const login: LoginPage = new LoginPage();
-let baseP = new base;
+let BasePage = new basePage;
 import { LoginPageUI } from '../interface/loginPageUI';
 
 const loginPage: LoginPageUI = new LoginPageUI();
 defineSupportCode(function ({ Given, When, Then }) {
-    // Then(/^I input company "(.*?)"$/, (value) => {
-    //     return login.typeInputCompany(value.toString());
-    // });
-
     Then(/^I input company "(.*?)"$/,async (value) => {
-        baseP.waitForElementAndSendkeys(loginPage.companyTextbox, value);
+        BasePage.waitForElementAndSendkeys(loginPage.companyTextbox, value);
     });
 
     Then(/^I input username to login "(.*?)"$/, (value) => {
-        return login.typeInputUsername(value.toString());
+        BasePage.waitForElementAndSendkeys(loginPage.usernameTextbox, value);
     });
 
     Then(/^I input password to login "(.*?)"$/, (value) => {
-        return login.typeInputPassword(value.toString());
+        BasePage.waitForElementAndSendkeys(loginPage.passwordTextbox, value);
     });
 
     Then(/^I click log in button$/, async() => {
         await login.clickToLoginBtn();
+        // await BasePage.waitForElementAndClick(loginPage.loginBtn)
+    
     });
 });
